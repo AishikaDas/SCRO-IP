42fdf256e564b69a2b97cea2e2c5bf4ba26afe26
AishikaDas <54441080+AishikaDas@users.noreply.github.com>
2019-08-23 22:55:52 +0530

    speed detection 2

---
+++ speed2.py
@@ -0,0 +1,123 @@
+import cv2
+import dlib
+import time
+import threading
+import math
+
+# Haarcascade classifier to identify vehicles
+carCascade = cv2.CascadeClassifier('myhaar.xml')
+video = cv2.VideoCapture('4.mp4') #video
+
+
+WIDTH = 1280
+HEIGHT = 720
+
+#define points to calculate speed
+def estimateSpeed(location1, location2):
+    #calculating pixels between two points
+	d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
+	# ppm = location2[2] / carWidht
+	ppm = 8.8 #pixels per metre
+	d_meters = d_pixels / ppm #distance in metres
+	#print("d_pixels=" + str(d_pixels), "d_meters=" + str(d_meters))
+	fps = 18 #frames per second
+	speed = d_meters * fps * 3.6
+	return speed
+def trackMultipleObjects():
+	rectangleColor = (0, 255, 0)
+	frameCounter = 0
+	currentCarID = 0
+	fps = 0
+	
+	carTracker = {}
+	carNumbers = {}
+	carLocation1 = {}
+	carLocation2 = {}
+	speed = [None] * 1000
+	
+	# Write output to video file
+	out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (WIDTH,HEIGHT))
+    
+
+
+	while True:
+		start_time = time.time()
+		rc, image = video.read()
+		if type(image) == type(None):
+			break
+		
+		image = cv2.resize(image, (WIDTH, HEIGHT))
+		resultImage = image.copy()
+		
+		frameCounter = frameCounter + 1
+        
+        for carID in carTracker.keys():
+			trackingQuality = carTracker[carID].update(image)
+        
+        if not (frameCounter % 10):
+			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+			cars = carCascade.detectMultiScale(gray, 1.1, 13, 18, (24, 24))
+            
+            for (_x, _y, _w, _h) in cars:
+				x = int(_x)
+				y = int(_y)
+				w = int(_w)
+				h = int(_h)
+			
+				x_bar = x + 0.5 * w
+				y_bar = y + 0.5 * h
+                
+                for carID in carTracker.keys():
+					trackedPosition = carTracker[carID].get_position()
+					
+					t_x = int(trackedPosition.left())
+					t_y = int(trackedPosition.top())
+					t_w = int(trackedPosition.width())
+					t_h = int(trackedPosition.height())
+					
+					t_x_bar = t_x + 0.5 * t_w
+					t_y_bar = t_y + 0.5 * t_h
+                    
+                    
+         for carID in carTracker.keys():
+			trackedPosition = carTracker[carID].get_position()
+					
+			t_x = int(trackedPosition.left())
+			t_y = int(trackedPosition.top())
+			t_w = int(trackedPosition.width())
+			t_h = int(trackedPosition.height())
+			
+			cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 4)
+			
+			# speed estimation
+			carLocation2[carID] = [t_x, t_y, t_w, t_h]
+		
+		end_time = time.time()
+		
+		if not (end_time == start_time):
+			fps = 1.0/(end_time - start_time)
+            
+        for i in carLocation1.keys():	
+			if frameCounter % 1 == 0:
+				[x1, y1, w1, h1] = carLocation1[i]
+				[x2, y2, w2, h2] = carLocation2[i]
+                
+                carLocation1[i] = [x2, y2, w2, h2]
+                
+                if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
+					if (speed[i] == None or speed[i] == 0) and y1 >= 275 and y1 <= 285:
+						speed[i] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2])
+
+					if speed[i] != None and y1 >= 180:
+						cv2.putText(resultImage, str(int(speed[i])) + " km/hr", (int(x1 + w1/2), int(y1-5)),cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+					
+		cv2.imshow('result', resultImage)
+
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	trackMultipleObjects()
